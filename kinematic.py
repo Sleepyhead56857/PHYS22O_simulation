eb8ca40ebeeaf65bc7d7442dfa8745a854c2e680
Sleepyhead56857 <149196831+Sleepyhead56857@users.noreply.github.com>
2024-04-09 10:55:08 -0400

    Add files via upload

---
+++ kinematic.py
@@ -0,0 +1,20 @@
+"""
+Author: Kelechi Agommuo, David Olufade, Rajjya Rhan Paudyal
+ID Numbers: XM33737, HP56857, SR3095
+Created: 9 April 2024
+
+
+
+"""
+import numpy as np
+from enum import Enum
+
+
+# Varibles that can be change by the user such as the object mass, enviroment (air resistance ,graviattional force, friction)    
+MASS = 0
+
+EARTH = 1.293 #kg m^-3
+MOON = 0
+MARS = 0.020 #kg m^ -3
+VENUS = 65 # kg m^-3
+

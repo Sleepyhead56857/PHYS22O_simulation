25757d6c9232aed76cdbc0f0d3cf6c7d756f9391
Sleepyhead56857 <149196831+Sleepyhead56857@users.noreply.github.com>
2024-04-16 09:49:54 -0400

    Add files via upload

--- kinematic.py
+++ kinematic.py
@@ -12,9 +12,32 @@ from enum import Enum
 
 # Varibles that can be change by the user such as the object mass, enviroment (air resistance ,graviattional force, friction)    
 MASS = 0
+V_INIT = 0
 
-EARTH = 1.293 #kg m^-3
-MOON = 0
-MARS = 0.020 #kg m^ -3
-VENUS = 65 # kg m^-3
+# Air density of the planet 
+EARTH_AIR_DENSITY = 1.293 #kg m^-3
+MOON_AIR_DENSITY = 0
+MARS_AIR_DENSITY = 0.020 #kg m^ -3
+VENUS_AIR_DENSITY = 65 # kg m^-3
+JUPITER_AIR_DENSITY = 0.16 # kg m^-3
 
+#Gravity (#m/s^2) from  https://www.smartconversion.com/factsheet/solar-system-gravity-of-planets
+EARTH_GRAVITY = 9.81    #m/s^2
+MOON_GRAVITY = 1.6      #m/s^2
+MARS_GRAVITY = 3.71     #m/s^2
+VENUS_GRAVITY = 8.9     #m/s^2
+JUPITER_GRAVITY = 23.1  #m/s^2
+
+
+# need to add function to account for
+    #   DRAG
+    #   WEIGHT
+    #   VELOCITY AT A CERTAIN TIME
+    #   POSITION AT A CERTAIN TIME
+    #   ACCELERATION AT A  CERTAIN TIME
+
+# DEVELOPED USER INTERACTION TO CHANGE THE VARAIBLE 
+
+# PLOT THE GIVEN INFORMATION
+
+# SOME HOW GET THE WEBSITE THE INFORMATION

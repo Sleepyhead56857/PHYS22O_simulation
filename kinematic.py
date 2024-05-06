b4540da5ccc33bd796d46b7b6ba544aba1670329
Sleepyhead56857 <149196831+Sleepyhead56857@users.noreply.github.com>
2024-05-05 21:42:41 -0400

    Add files via upload

--- kinematic.py
+++ kinematic.py
@@ -10,16 +10,21 @@ import numpy as np
 from enum import Enum
 
 
-# Varibles that can be change by the user such as the object mass, enviroment (air resistance ,graviattional force, friction)    
+# Varibles that can be change by the user such as the object mass, enviroment (air resistance ,graviattional force, friction)
 MASS = 0
 V_INIT = 0
+ANGLE_INIT = 0
+POS_INIT = np.array([0,0], float)
+SIZE = 
+RADIUS = 0.5 * SIZE
 
-# Air density of the planet 
+# Air density of the planet
 EARTH_AIR_DENSITY = 1.293 #kg m^-3
 MOON_AIR_DENSITY = 0
 MARS_AIR_DENSITY = 0.020 #kg m^ -3
 VENUS_AIR_DENSITY = 65 # kg m^-3
 JUPITER_AIR_DENSITY = 0.16 # kg m^-3
+all_densities = np.array([1.293, 0, 0.020, 64, 0.16]) # Putting all gravities from current densities into list
 
 #Gravity (#m/s^2) from  https://www.smartconversion.com/factsheet/solar-system-gravity-of-planets
 EARTH_GRAVITY = 9.81    #m/s^2
@@ -27,16 +32,79 @@ MOON_GRAVITY = 1.6      #m/s^2
 MARS_GRAVITY = 3.71     #m/s^2
 VENUS_GRAVITY = 8.9     #m/s^2
 JUPITER_GRAVITY = 23.1  #m/s^2
+all_gravities = np.array([9.81, 1.6, 3.71, 8.9, 23.1]) # Putting all gravities from current planets into list
+
+#Drag coeffiencinnt from https://www1.grc.nasa.gov/beginners-guide-to-aeronautics/shape-effects-on-drag-2/
+SPHERE_DRAG_COE = .5
+AIRFOIL_DRAG_COE = .045
+BULLET_DRAG_COE = .295
+FLAT_PLATE_DRAG_COE = 1.28
+PRISM_DRAG_COE = 1.14
+
+#
+SA_Sphere = 4 * np.pi * SIZE**2
+
+SA_BULLET_CONE = np.pi * RADIUS * np.sqrt(SIZE**2 + RADIUS**2)
+SA_BULLET_CYL = 2 * np * RADIUS * SIZE + np.pi * RADIUS**2
+SA_BULLET = SA_BULLET_CONE + SA_BULLET_CYL
+
+#
+curr_velo = 0.0
+
+
+# List to hold selected planet information
+selected_planet = []
+selected_shape = []
+
+
+
+
 
 
 # need to add function to account for
     #   DRAG
-    #   WEIGHT
-    #   VELOCITY AT A CERTAIN TIME
-    #   POSITION AT A CERTAIN TIME
-    #   ACCELERATION AT A  CERTAIN TIME
+    def drag_at_time(curr_velo):
 
-# DEVELOPED USER INTERACTION TO CHANGE THE VARAIBLE 
+        v = pos / MASS
+
+        #F_air = -1/2 * p(air density) * Area * drag_Coeffeient * | v |^2 * unit_vector
+
+        F_air = - selected_shape * selected_planet * selected_shape *((curr_velo**2)/2) 
+
+        #When Weight == Drag it has reah ter,inal velocty https://www.grc.nasa.gov/www/k-12/VirtualAero/BottleRocket/airplane/termv.html
+
+
+    #   GRAVITY
+    def Force_Gravity():
+        F_gravity = MASS * selected_planet
+
+        return F_gravity
+
+
+    #   WEIGHT
+    def net_force(obj_weight):
+        return (obj_weight * selected_planet[1])
+    #   VELOCITY AT A CERTAIN TIME
+    def velocity_at_time(time):
+        pass
+
+    #   POSITION AT A CERTAIN TIME
+    def position_at_time(time):
+        pass
+    #   ACCELERATION AT A  CERTAIN TIME
+    def accel_at_time(time):
+        pass
+
+# DEVELOPED USER INTERACTION TO CHANGE THE VARAIBLE
+# User Input
+user_inp = input("What planet would you like to select? (1-5)")
+
+# Selecting planet info to append into list based on user choice
+selected_planet.append(all_densities[int(user_inp) - 1])
+selected_planet.append(all_gravities[int(user_inp) - 1])
+
+MASS = float(input("Enter the object's mass (in kg): "))
+V_INIT = float(input("Enter the initial velocity (in m/s): "))
 
 # PLOT THE GIVEN INFORMATION
 
